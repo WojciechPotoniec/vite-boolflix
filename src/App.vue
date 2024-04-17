77890c19c7084f5fa6464c8b2f997bd0525aa95c
wojciechpotoniec <wojtekpotoniec@hotmail.it>
2024-04-17 12:03:13 +0200

    import axios and store with API

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,36 @@
-<template>
-  
-</template>
+<template>AppVue</template>
 
 <script>
-  export default {
-    name: 'App'
-  }
+import axios from "axios";
+import { store } from "./store";
+export default {
+  name: "App",
+  data() {
+    return {
+      store,
+    };
+  },
+  methods: {
+    getMovies() {
+      axios
+        .get(this.store.baseUrl + this.store.endpoint.movie, this.store.options)
+        .then((res) => {
+          console.log(res.data.results);
+        });
+    },
+    getTvSeries() {
+      axios
+        .get(this.store.baseUrl + this.store.endpoint.tv, this.store.options)
+        .then((res) => {
+          console.log(res.data.results);
+        });
+    },
+  },
+  created() {
+    // this.getMovies();
+    // this.getTvSeries(); test fatto tutto ok
+  },
+};
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
