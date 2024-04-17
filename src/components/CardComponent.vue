64be444b8d80d6c2e372cb839fa6146686111839
wojciechpotoniec <wojtekpotoniec@hotmail.it>
2024-04-17 13:56:44 +0200

    created CardComponent

---
+++ src/components/CardComponent.vue
@@ -0,0 +1,22 @@
+<template>
+    <CardList />
+</template>
+
+<script>
+import CardList from "./CardList.vue";
+    export default {
+        name: "CardComponent",
+        components: {
+            CardList,
+        },
+        data() {
+            return{
+
+            }
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
