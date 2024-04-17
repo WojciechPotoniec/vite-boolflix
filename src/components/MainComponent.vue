4f2069f889cc5dfd7661e216bb9d7e37ee3873c1
wojciechpotoniec <wojtekpotoniec@hotmail.it>
2024-04-17 13:16:25 +0200

    created MainComponent

---
+++ src/components/MainComponent.vue
@@ -0,0 +1,23 @@
+<template>
+    <Main>
+        
+    </Main>
+</template>
+
+<script>
+    export default {
+        name: "MainComponent",
+        data() {
+            return {
+                
+            }
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+main{
+    height: calc(100vh - 79px);
+    background-color: #434343;
+}
+</style>
