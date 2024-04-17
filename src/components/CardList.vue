51d8bb72461edd0e7c9096061bd8e398bf2c7a74
wojciechpotoniec <wojtekpotoniec@hotmail.it>
2024-04-17 13:56:30 +0200

    created CardList

---
+++ src/components/CardList.vue
@@ -0,0 +1,18 @@
+<template>
+    CardList
+</template>
+
+<script>
+    export default {
+        name: "CardList",
+        data() {
+            return {
+                
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
