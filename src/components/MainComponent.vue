327b49297dd4771476484566ef5ad77f714d3667
wojciechpotoniec <wojtekpotoniec@hotmail.it>
2024-04-17 13:56:15 +0200

    fix

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,17 +1,21 @@
 <template>
-    <Main>
-        
-    </Main>
+    <main>
+        <CardComponent />
+    </main>
 </template>
 
 <script>
+import CardComponent from "./CardComponent.vue";
     export default {
         name: "MainComponent",
+        components: {
+            CardComponent,
+        },
         data() {
             return {
                 
             }
-        },
+        }
     }
 </script>
 
