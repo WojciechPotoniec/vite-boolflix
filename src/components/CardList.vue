c43bbeb747c2fde5c43a3148c45d2f32287abd1b
wojciechpotoniec <wojtekpotoniec@hotmail.it>
2024-04-17 18:28:36 +0200

    update cardlist component

--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -1,14 +1,28 @@
 <template>
   <div class="container">
-    <div class="row">
+    <div class="row gx-0 rounded-0">
+      <h2 class="my-3 text-light">Film</h2>
       <div
-        class="col-12 col-lg-2 col-md-3 col-sm-6 g-3 gy-4"
+        class="col-12 col-lg-2 col-md-3 col-sm-6"
         v-for="movie in store.movieList"
         :key="movie.id"
       >
         <CardComponent
           :img="store.imageUrl + movie.poster_path"
           :title="movie.title"
+        />
+      </div>
+    </div>
+    <div class="row gx-0">
+      <h2 class="my-3 text-light">Serie tv</h2>
+      <div
+        class="col-12 col-lg-2 col-md-3 col-sm-6"
+        v-for="series in store.seriesList"
+        :key="series.id"
+      >
+        <CardComponent
+          :img="store.imageUrl + series.poster_path"
+          :title="series.title"
         />
       </div>
     </div>
