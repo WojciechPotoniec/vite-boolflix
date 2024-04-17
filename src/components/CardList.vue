4a0cfc6a537c8137560273f23047754d2b558410
wojciechpotoniec <wojtekpotoniec@hotmail.it>
2024-04-17 17:46:57 +0200

    created template with dinamic props rules

--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -1,18 +1,34 @@
 <template>
-    CardList
+  <div class="container">
+    <div class="row">
+      <div
+        class="col-12 col-lg-2 col-md-3 col-sm-6 g-3 gy-4"
+        v-for="movie in store.movieList"
+        :key="movie.id"
+      >
+        <CardComponent
+          :img="store.imageUrl + movie.poster_path"
+          :title="movie.title"
+        />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "CardList",
-        data() {
-            return {
-                
-            }
-        }
-    }
+import { store } from "../store.js";
+import CardComponent from "./CardComponent.vue";
+export default {
+  name: "CardList",
+  components: {
+    CardComponent,
+  },
+  data() {
+    return {
+      store,
+    };
+  },
+};
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
